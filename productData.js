a80c845008530d50e7b39eefdd275649cb571b9c
webdesignertsmsby <webdesigner.tsmsby@gmail.com>
2025-12-30 13:27:47 +0700

    Create productData.js with initial product list
    Added product data with details for three machines.

---
+++ productData.js
@@ -0,0 +1,27 @@
+// productData.js
+const products = [
+    {
+        id: 1,
+        name: "Nama Mesin 1",
+        category: "Kategori A",
+        price: 12500000,
+        image: "https://i.imgur.com/ulxVgcM.jpeg", // Gunakan URL gambar aslimu
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 2,
+        name: "Nama Mesin 2",
+        category: "Kategori B",
+        price: 18000000,
+        image: "https://i.imgur.com/aR65qAp.jpeg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 3,
+        name: "Nama Mesin 3",
+        category: "Kategori C",
+        price: 8500000,
+        image: "https://i.imgur.com/tzU2IJL.jpeg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+];
