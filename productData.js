1f559bf7c26135c8be1772fe0e94c181afc7b267
webdesignertsmsby <webdesigner.tsmsby@gmail.com>
2026-01-06 13:39:10 +0700

    Update product categories to new classifications

--- productData.js
+++ productData.js
@@ -3,7 +3,7 @@ const products = [
     {
         id: 1,
         name: "Nama Mesin 1",
-        category: "Kategori A",
+        category: "BAKERY",
         price: 12500000,
         image: "https://i.imgur.com/PnDdnvZ.png", // Gunakan URL gambar aslimu
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
@@ -11,7 +11,7 @@ const products = [
     {
         id: 2,
         name: "Nama Mesin 2",
-        category: "Kategori B",
+        category: "CAFE",
         price: 18000000,
         image: "https://i.imgur.com/PnDdnvZ.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
@@ -19,7 +19,7 @@ const products = [
     {
         id: 3,
         name: "Nama Mesin 3",
-        category: "Kategori C",
+        category: "HOTEL",
         price: 8500000,
         image: "https://i.imgur.com/PnDdnvZ.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
