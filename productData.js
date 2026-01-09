9d215deed2392d2226c06978e4024cd9c111a090
webdesignertsmsby <webdesigner.tsmsby@gmail.com>
2026-01-09 16:22:53 +0700

    Update productData.js

--- productData.js
+++ productData.js
@@ -2,26 +2,106 @@
 const products = [
     {
         id: 1,
-        name: "Nama Mesin 1",
+        name: "Oven Single Deck",
         category: "Bakery",
-        price: 12500000,
-        image: "https://i.imgur.com/PnDdnvZ.png", // Gunakan URL gambar aslimu
+        price: 000000,
+        image: "https://i.imgur.com/50yWcuA.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         id: 2,
-        name: "Nama Mesin 2",
-        category: "Cafe",
-        price: 18000000,
-        image: "https://i.imgur.com/PnDdnvZ.png",
+        name: "Oven Double Deck",
+        category: "Bakery",
+        price: 000000,
+        image: "https://i.imgur.com/GZWWM0r.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         id: 3,
-        name: "Nama Mesin 3",
-        category: "Hotel",
-        price: 8500000,
-        image: "https://i.imgur.com/PnDdnvZ.png",
+        name: "Proofer",
+        category: "Bakery",
+        price: 000000,
+        image: "https://i.imgur.com/UfklByO.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    }
+    },
+    {
+        id: 4,
+        name: "Meja Tray",
+        category: "Bakery",
+        price: 000000,
+        image: "https://i.imgur.com/y1tbsuV.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 5,
+        name: "Blender",
+        category: "Cafe",
+        price: 000000,
+        image: "https://i.imgur.com/17kvXW8.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 6,
+        name: "Meja Bar",
+        category: "Cafe",
+        price: 000000,
+        image: "https://i.imgur.com/VVBUOps.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 7,
+        name: "Mesin Es Serut",
+        category: "Cafe",
+        price: 000000,
+        image: "https://i.imgur.com/zi98cfA.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 8,
+        name: "Mesin Cup Sealer",
+        category: "Cafe",
+        price: 000000,
+        image: "https://i.imgur.com/tCc5ry2.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 9,
+        name: "Kitchen Set",
+        category: "Hotel",
+        price: 000000,
+        image: "https://i.imgur.com/ukogfdK.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 10,
+        name: "Freezer",
+        category: "Hotel",
+        price: 000000,
+        image: "https://i.imgur.com/eZwq6tu.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 11,
+        name: "Kulkas",
+        category: "Hotel",
+        price: 000000,
+        image: "https://i.imgur.com/r0nlosP.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 12,
+        name: "Tray",
+        category: "Aksesoris",
+        price: 000000,
+        image: "https://i.imgur.com/IQygyDS.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: 13,
+        name: "Sarung Tangan",
+        category: "Aksesoris",
+        price: 000000,
+        image: "https://i.imgur.com/tzNHDKr.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
 ];
