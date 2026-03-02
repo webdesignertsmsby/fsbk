477f5b1824160a7102c04cbe485387b867da963b
webdesignertsmsby <webdesigner.tsmsby@gmail.com>
2026-03-02 14:04:13 +0700

    Update productData.js

--- productData.js
+++ productData.js
@@ -4,7 +4,7 @@ const products = [
         id: 1,
         name: "Oven Single Deck",
         category: "Bakery",
-        price: 7000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/z5ZtZK8.png", "https://i.imgur.com/tizJB8F.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -12,7 +12,7 @@ const products = [
         id: 2,
         name: "Oven Double Deck",
         category: "Bakery",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/DigWh5J.png", "https://i.imgur.com/nCikQaF.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -20,7 +20,7 @@ const products = [
         id: 3,
         name: "Proofer",
         category: "Bakery",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/SpF2rui.png", "https://i.imgur.com/mQbjmg5.png" ], 
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -28,7 +28,7 @@ const products = [
         id: 4,
         name: "Meja Tray",
         category: "Bakery",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/gwYXsER.png" , "https://i.imgur.com/cPsEVOh.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -36,7 +36,7 @@ const products = [
         id: 5,
         name: "Blender",
         category: "Cafe",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/eDWEjQR.png", "https://i.imgur.com/kvtTwLs.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -44,7 +44,7 @@ const products = [
         id: 6,
         name: "Meja Bar",
         category: "Cafe",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/ZtMfxnp.png", "https://i.imgur.com/ugMMFsj.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -52,7 +52,7 @@ const products = [
         id: 7,
         name: "Mesin Es Serut",
         category: "Cafe",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/AoR9ZV0.png", "https://i.imgur.com/RrBOwJF.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -60,7 +60,7 @@ const products = [
         id: 8,
         name: "Mesin Cup Sealer",
         category: "Cafe",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/lyLsAwA.png", "https://i.imgur.com/KlLfYMm.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -68,7 +68,7 @@ const products = [
         id: 9,
         name: "Kitchen Set",
         category: "Hotel",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/ukogfdK.png", "https://i.imgur.com/A6skIkX.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -76,7 +76,7 @@ const products = [
         id: 10,
         name: "Freezer",
         category: "Hotel",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/PZIqdsF.png", "https://i.imgur.com/SDx9i06.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -84,7 +84,7 @@ const products = [
         id: 11,
         name: "Kulkas",
         category: "Hotel",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/RLnuWML.png", "https://i.imgur.com/94pWZaL.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -92,7 +92,7 @@ const products = [
         id: 12,
         name: "Tray",
         category: "Aksesoris",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/HJ4NGA6.png", "https://i.imgur.com/2QNYnYs.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
@@ -100,7 +100,7 @@ const products = [
         id: 13,
         name: "Sarung Tangan",
         category: "Aksesoris",
-        price: 000000,
+        price: 9999999999,
         image: [ "https://i.imgur.com/U8S17RM.png", "https://i.imgur.com/I8Op4x8.png" ],
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
